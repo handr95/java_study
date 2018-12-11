b9653f52140f374947a21f6b7ae8def792d63cb2
handr95 <handr95@naver.com>
2018-12-11 14:01:10 +0900

    스프링 프로젝트 구성

---
+++ src/main/java/study/comtroller/HomeController.java
@@ -0,0 +1,13 @@
+package study.comtroller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "index";
+    }
+}
