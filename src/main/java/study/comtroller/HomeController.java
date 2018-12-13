599e4c1e2b8e4bf489bca80855f313df00d0d047
handr95 <handr95@naver.com>
2018-12-13 16:33:33 +0900

    브랜치 테스트

--- src/main/java/study/comtroller/HomeController.java
+++ src/main/java/study/comtroller/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController {
     public String home() {
         return "index";
     }
+
+    @RequestMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
